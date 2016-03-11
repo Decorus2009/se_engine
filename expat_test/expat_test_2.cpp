37c13a15235fd3b9b0b63df2c4d829d9b5da34ca
Vitalii Ushanov <Decorus2009@yandex.ru>
2016-03-11 03:06:47 +0300

    expat testing

---
+++ expat_test/expat_test_2.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <expat.h>
+#include <iostream>
+
+#define BUFFSIZE 1024	
+char Buff[BUFFSIZE];
+int depth;
+XML_Parser first;
+
+static void XMLCALL start(void *user_data, const char *tag, const char **attr)
+{
+    int i;
+    for (i = 0; i < depth; i++) {
+		printf("\t");
+	}	
+    
+	printf("tag = %s", tag);
+
+    for (i = 0; attr[i]; i += 2) {
+		printf(" : attrib %s='%s'\n", attr[i], attr[i + 1]);
+    }
+
+    ++depth;
+}
+
+static void XMLCALL end(void *userdata, const char *tag) { --depth; }
+
+
+void text_handler(void *data, const char *txt, int txtlen) {
+  	printf("\n%4d: Text - ", depth++);
+  	fwrite(txt, txtlen, sizeof(char), stdout);
+}
+
+int main (void) {
+	
+	FILE * f = fopen("simple_xml_test.xml", "r");
+    int done, len;
+    first = XML_ParserCreate(NULL);
+    XML_SetElementHandler(first, start, end);
+	XML_SetCharacterDataHandler(first, text_handler);
+    
+	do {
+		len = fread(Buff, 1, BUFFSIZE, f);
+		done = feof(f);
+		XML_Parse(first, Buff, len, done);
+    } 
+	while (!done);
+    
+	XML_ParserFree(first);
+    return 0;
+}
